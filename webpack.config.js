d90599f992c86825a638971ca223c1f2986b55f1
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-08 23:23:47 +0300

    modify config files for html loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { title } = require('process')
 module.exports = {
   mode: 'development',
   entry: path.resolve(__dirname, 'src'),
@@ -20,6 +19,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'To do list',
       filename: 'index.html',
+      template: 'src/template.html',
     }),
   ],
 }
