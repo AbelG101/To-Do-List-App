d85cd76ef5820482eaa2595c310ef3c5596713e2
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-08 23:21:38 +0300

    add config files for html loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { title } = require('process')
 module.exports = {
   mode: 'development',
   entry: path.resolve(__dirname, 'src'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
-    rules: [{
+    rules: [
+      {
       test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader'
-      ]
-    }]
-  }
+      use: ['style-loader', 'css-loader'],
+    },
+  ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'To do list',
+      filename: 'index.html',
+    }),
+  ],
 }
