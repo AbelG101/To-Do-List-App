6b65298c2e4a0e98ae3de64dd17cc0ec41273a57
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-08 23:50:19 +0300

    add config for dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,16 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
   module: {
     rules: [
       {
