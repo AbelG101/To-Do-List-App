605ff6a6a81b39c3f5d2cc909057dd409384d76b
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-11 17:35:49 +0300

    modified update-task-status function code

--- src/JS/statusUpdates.js
+++ src/JS/statusUpdates.js
@@ -3,8 +3,8 @@ import { tasksList } from "./taskOperations.js";
 const updateTaskStatus = (index) => {
   if (tasksList[index].completed === true) {
     tasksList[index].completed = false;
-  }
-  else {
+  } else {
+    console.log(tasksList[index]);
     tasksList[index].completed = true;
   }
   localStorage.setItem('tasks: ', JSON.stringify(tasksList));
