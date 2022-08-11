fd50decc1f167468f3db06a51ca5ad3bfb4809da
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-11 13:34:43 +0300

    add code for updating the completed tasks status

---
+++ src/JS/statusUpdates.js
@@ -0,0 +1,13 @@
+import { tasksList } from "./taskOperations.js";
+
+const updateTaskStatus = (index) => {
+  if (tasksList[index].completed === true) {
+    tasksList[index].completed = false;
+  }
+  else {
+    tasksList[index].completed = true;
+  }
+  localStorage.setItem('tasks: ', JSON.stringify(tasksList));
+};
+
+export default updateTaskStatus;
