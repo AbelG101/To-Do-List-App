71f829e42cbf023af90ba94acf642f4df78a7642
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-16 17:46:45 +0300

    improved taskStatus function

--- src/JS/statusUpdates.js
+++ src/JS/statusUpdates.js
@@ -2,11 +2,7 @@ import { getTasksList } from './taskOperations.js';
 
 const updateTaskStatus = (index) => {
   const tasksList = getTasksList();
-  if (tasksList[index].completed === true) {
-    tasksList[index].completed = false;
-  } else {
-    tasksList[index].completed = true;
-  }
+  tasksList[index].completed = !tasksList[index].completed;
   localStorage.setItem('tasks: ', JSON.stringify(tasksList));
 };
 
