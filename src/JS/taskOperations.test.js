ac7fd9bd0e5ecb03e72c1a23b1a46955dbfc3f4c
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-17 13:43:28 +0300

    add test for remove task

--- src/JS/taskOperations.test.js
+++ src/JS/taskOperations.test.js
@@ -38,3 +38,47 @@ class LocalStorageMock {
 }
 
 global.localStorage = new LocalStorageMock;
+
+describe('check for add-delete operations', () => {
+  
+  test('properly remove task items', () => {
+    document.body.innerHTML = `<div class="tasks-container">
+    <div class="task">
+      <div class="task-content">
+        <input type="checkbox"><input class="task-value" type="text" value="task-a">
+      </div>
+        <i class="fa fa-trash trash-icon" aria-hidden="true"></i><i class="fa fa-ellipsis-v dots-icon" aria-hidden="true"></i>
+    </div>
+    <div class="task">
+      <div class="task-content">
+        <input type="checkbox"><input class="task-value" type="text" value="task-b">
+      </div>
+      <i class="fa fa-trash trash-icon" aria-hidden="true"></i><i class="fa fa-ellipsis-v dots-icon" aria-hidden="true"></i>
+    </div>
+    <div class="task">
+      <div class="task-content">
+        <input type="checkbox"><input class="task-value" type="text" value="task-c">
+      </div>
+      <i class="fa fa-trash trash-icon" aria-hidden="true"></i><i class="fa fa-ellipsis-v dots-icon" aria-hidden="true"></i>
+    </div>
+  </div>`
+  addTaskToArray('task-a');
+  addTaskToArray('task-b');
+  addTaskToArray('task-c');
+  let indexOfRemovedTask = 1;
+  removeTask(indexOfRemovedTask);
+  let tasksList = getTasksList();
+  let tasksValueElt = document.querySelectorAll('.task-value')[indexOfRemovedTask];
+  expect(tasksList[indexOfRemovedTask].description).toBe('task-c');
+  expect(tasksList[indexOfRemovedTask].index).toEqual(indexOfRemovedTask);
+  expect(tasksValueElt.value).toBe('task-c');
+
+  removeTask(indexOfRemovedTask);
+  tasksList = getTasksList();
+  tasksValueElt = document.querySelectorAll('.task-value')[indexOfRemovedTask - 1];
+  expect(tasksList[indexOfRemovedTask - 1].description).toBe('task-a');
+  expect(tasksList[indexOfRemovedTask - 1].index).toEqual(indexOfRemovedTask - 1);
+  expect(tasksValueElt.value).toBe('task-a');
+  });
+
+});
