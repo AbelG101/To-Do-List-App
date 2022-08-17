8bfa4d7035c0fd75a23f82f664174a4a0a555c69
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-17 13:41:01 +0300

    add a localstorage mock

--- src/JS/taskOperations.test.js
+++ src/JS/taskOperations.test.js
@@ -14,3 +14,27 @@
   clearCompletedTasks,
   addTaskToArray,
 } from './taskOperations';
+
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock;
