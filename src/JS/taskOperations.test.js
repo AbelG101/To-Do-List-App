7f07eeea2d7a5d7906d0fd2bb2a5821eb6417f0c
Inieke Sendy <ini.e.sendy@gmail.com>
2022-08-17 11:51:10 +0100

    [unit-test]: add: Adding Task testing operation

--- src/JS/taskOperations.test.js
+++ src/JS/taskOperations.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
- import {
+import {
   addTaskToPage,
   addOnLocalStorage,
   loadTasksFromLS,
@@ -13,7 +13,7 @@
   setTasksList,
   clearCompletedTasks,
   addTaskToArray,
-} from './taskOperations';
+} from "./taskOperations";
 
 class LocalStorageMock {
   constructor() {
@@ -37,11 +37,10 @@ class LocalStorageMock {
   }
 }
 
-global.localStorage = new LocalStorageMock;
+global.localStorage = new LocalStorageMock();
 
-describe('check for add-delete operations', () => {
-  
-  test('properly remove task items', () => {
+describe("check for add-delete operations", () => {
+  test("properly remove task items", () => {
     document.body.innerHTML = `<div class="tasks-container">
     <div class="task">
       <div class="task-content">
@@ -61,30 +60,34 @@ describe('check for add-delete operations', () => {
       </div>
       <i class="fa fa-trash trash-icon" aria-hidden="true"></i><i class="fa fa-ellipsis-v dots-icon" aria-hidden="true"></i>
     </div>
-  </div>`
-  addTaskToArray('task-a');
-  addTaskToArray('task-b');
-  addTaskToArray('task-c');
-  let indexOfRemovedTask = 1;
-  removeTask(indexOfRemovedTask);
-  let tasksList = getTasksList();
-  let tasksValueElt = document.querySelectorAll('.task-value')[indexOfRemovedTask];
-  expect(tasksList[indexOfRemovedTask].description).toBe('task-c');
-  expect(tasksList[indexOfRemovedTask].index).toEqual(indexOfRemovedTask);
-  expect(tasksValueElt.value).toBe('task-c');
+  </div>`;
+    addTaskToArray("task-a");
+    addTaskToArray("task-b");
+    addTaskToArray("task-c");
+    let indexOfRemovedTask = 1;
+    removeTask(indexOfRemovedTask);
+    let tasksList = getTasksList();
+    let tasksValueElt =
+      document.querySelectorAll(".task-value")[indexOfRemovedTask];
+    expect(tasksList[indexOfRemovedTask].description).toBe("task-c");
+    expect(tasksList[indexOfRemovedTask].index).toEqual(indexOfRemovedTask);
+    expect(tasksValueElt.value).toBe("task-c");
 
-  removeTask(indexOfRemovedTask);
-  tasksList = getTasksList();
-  tasksValueElt = document.querySelectorAll('.task-value')[indexOfRemovedTask - 1];
-  expect(tasksList[indexOfRemovedTask - 1].description).toBe('task-a');
-  expect(tasksList[indexOfRemovedTask - 1].index).toEqual(indexOfRemovedTask - 1);
-  expect(tasksValueElt.value).toBe('task-a');
+    removeTask(indexOfRemovedTask);
+    tasksList = getTasksList();
+    tasksValueElt =
+      document.querySelectorAll(".task-value")[indexOfRemovedTask - 1];
+    expect(tasksList[indexOfRemovedTask - 1].description).toBe("task-a");
+    expect(tasksList[indexOfRemovedTask - 1].index).toEqual(
+      indexOfRemovedTask - 1
+    );
+    expect(tasksValueElt.value).toBe("task-a");
   });
 
-  test('should add task to local storage', () => {
+  test("should add task to local storage", () => {
     let tasksList = getTasksList();
     const task = {
-      description: 'task1',
+      description: "task1",
       completed: false,
       index: 0,
     };
@@ -93,8 +96,21 @@ describe('check for add-delete operations', () => {
     expect(tasksList.length).toBeGreaterThanOrEqual(0);
   });
 
-  test('properly get list of tasks array of objects', () => {
+  test("properly get list of tasks array of objects", () => {
     const tasksList = getTasksList();
     expect(tasksList.length).toBeGreaterThanOrEqual(0);
+  });
+
+  test("properly adds the tasks to the page", () => {
+    const task = {
+      description: "task1",
+      completed: false,
+      index: 0,
+    };
+    document.body.innerHTML = `<div class="tasks-container">
+    </div>`;
+    addTaskToPage(task);
+    const tasksContainerElt = document.querySelectorAll(".task");
+    expect(tasksContainerElt).toHaveLength(1);
   });
 });
